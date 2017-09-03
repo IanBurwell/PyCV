d0251c0eb2bb55e2137cef2d776999bfc6944d39
Ian <spyguyian@gmail.com>
2017-09-03 15:51:25 -0400

    Started Making a image search engine for corgis and balloons

--- Projects/camshift.py
+++ Projects/camshift.py
@@ -23,8 +23,9 @@ while cv2.waitKey(10) != ord("n"):
 track_window = (w/2-100,h/2-100,200,200)
 roi = frame[h/2-100:h/2+100, w/2-100:w/2+100]
 hsv_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
-roi_hist = cv2.calcHist([hsv_roi],[0],None,[180],[0,180])
-cv2.normalize(roi_hist, roi_hist,0, 255,cv2.NORM_MINMAX)
+#roi_hist = cv2.calcHist([hsv_roi],[0],None,[180],[0,180])
+roi_hist = cv2.calcHist([hsv_roi],[0,1],None,[180,256],[0,180,0,256])
+cv2.normalize(roi_hist, roi_hist, 0, 240, cv2.NORM_MINMAX)
 
 terminationCriteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1)
 while True:
@@ -33,7 +34,8 @@ while True:
         break
 
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    backProj = cv2.calcBackProject([hsv], [0], roi_hist, [0, 180], 1)
+    #backProj = cv2.calcBackProject([hsv], [0], roi_hist, [0, 180], 1)
+    backProj = cv2.calcBackProject([hsv], [0,1 ], roi_hist, [0, 180, 0, 256], 1)
 
     r, track_window = cv2.CamShift(backProj, track_window, terminationCriteria)
     pts = cv2.boxPoints(r)
